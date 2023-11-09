a38694ade38964c7d681acc51277c45e12b98437
Stepanenko2021 <daniilsteponenko942@gmail.com>
2023-11-09 15:18:17 +0300

    ver2

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,30 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
    try( FileWriter fw= new FileWriter("Q://Java//file.txt")) {
-       String text = "hello gods";
+       System.out.println("Слова");
+       String text = "Hello \ngods";
+
        fw.write(text);
        fw.append('\n');
-
        fw.flush();
    }
    catch (IOException ex) {
         System.out.println(ex.getMessage());
    }
+
    try( FileReader fr = new FileReader("Q://Java//file.txt")) {
-        char[] buf = new char[256];
-        int c;
-
-        while((c = fr.read(buf))>0){
-            if(c < 256){
-                buf = Arrays.copyOf(buf, c);
-
-            }
-            System.out.println(buf);
-
-        }
-
+     Scanner scan = new Scanner(fr);
+     int i =1;
+     while (scan.hasNextLine()){
+         System.out.println(i + ":" + scan.nextLine());
+         i++;
+     }
+fr.close();
    }
    catch (IOException ex){
        System.out.println(ex.getMessage());
    }
+
     }
 
 
